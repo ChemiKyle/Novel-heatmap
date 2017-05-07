4bf6bf5d2cf4e1120b2194df214136bf23665bc9
kyle <kyle.2493@gmail.com>
2017-05-07 18:11:08 -0400

    Automate breaking up of a large book file

---
+++ split_book_to_chapters.py
@@ -0,0 +1,23 @@
+import tkinter
+
+def split_by_chapter(book_file):
+	chapter_split = book_file.split('Chapter ') # Obviously not perfect, but works in many cases
+	return chapter_split
+
+def write_chapters_to_files(chapter_split):
+	chap_num = 0
+	for i in chapter_split:
+		with open('ch' + str(chap_num) + '.txt', 'w') as w:
+			w.write(i)
+		chap_num += 1
+
+def main():
+    chosen_book_file = tkinter.filedialog.askopenfilename()
+    with open(chosen_book_file, 'r') as r:
+        book_file = r.read()
+
+	chapter_split = split_by_chapter(book_file)
+	write_chapters_to_files(chapter_split)
+
+if __name__ == '__main__':
+	main()
