f7e4cdf72626b25c09a40f9e88702d21f758cbf7
kyle <kyle.2493@gmail.com>
2017-05-08 15:30:26 -0400

    PEP8 compliance

--- book_map.py
+++ book_map.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os, tkinter, re
+import os
+import tkinter
+import re
 
 
 def make_bookmap(book_text):
@@ -8,8 +10,8 @@ def make_bookmap(book_text):
     longest_line = 0
     line_map = re.split('[\.\?\!]', book_text)
 
-    for line in line_map[:-1]: # Skip the last entry since it's a space after the final period
-        this_line_map = line.replace('"', '').replace(',','').replace(';','').replace("\n",'').split(' ')
+    for line in line_map[:-1]:  # Skip the last entry since it's a space after the final period
+        this_line_map = line.replace('"', '').replace(',', '').replace(';', '').replace("\n", '').split(' ')
         line_count_map = []
 
         for word in this_line_map:
@@ -24,6 +26,7 @@ def make_bookmap(book_text):
 
     return book_map, longest_line
 
+
 def pad_vector_with_zeroes(book_map, longest_line):
     for line in book_map:
         word_count = len(line)
@@ -31,7 +34,7 @@ def pad_vector_with_zeroes(book_map, longest_line):
             for i in range(longest_line - word_count):
                 line.append(0)
 
-directly_to_figure = True # Set to false to produce a preview first
+
 def numpy_image_maker(book_map, book_name):
     a = np.asarray(book_map)
     a = np.transpose(a)
@@ -41,8 +44,9 @@ def numpy_image_maker(book_map, book_name):
     cmap = 'Blues_r'
     # More info at: https://matplotlib.org/users/colormaps.html
 
-    dpi = 100 # Default for this with direct write is 80, but images our size don't warrant compression
-    plt.imshow(a, cmap=cmap ,interpolation='nearest')
+    dpi = 100  # Default for this with direct write is 80, but images our size don't warrant compression
+    plt.imshow(a, cmap=cmap, interpolation='nearest')
+    directly_to_figure = True  # Set to false to produce a preview first
     if directly_to_figure:
         plt.savefig(book_name + '_map_image.png', bbox_inches='tight', dpi=dpi)
     else:
@@ -55,6 +59,7 @@ def matrix_maker(book_map, book_name):
     a = np.transpose(a)
     np.savetxt(book_name + ".mat", a, newline=";\n")
 
+
 def process_to_dictionary(chosen_file, chapter_dict):
     book_name, ext = os.path.splitext(chosen_file)
     with open(chosen_file, 'r') as f:
@@ -64,11 +69,20 @@ def process_to_dictionary(chosen_file, chapter_dict):
     chapter_dict[book_name] = book_map
     pad_vector_with_zeroes(book_map, longest_line)
 
+
 def process_chapters_to_images(chapter_dict):
     for i in chapter_dict:
         file_name = i
         book_map = chapter_dict[i]
         numpy_image_maker(book_map, file_name)
+
+
+def process_chapters_to_matrices(chapter_dict):
+    for i in chapter_dict:
+        file_name = i
+        book_map = chapter_dict[i]
+        matrix_maker(book_map, file_name)
+
 
 def normalize_all_maps(chapter_dict):
     longest_sentence = 0
@@ -85,18 +99,20 @@ def normalize_all_maps(chapter_dict):
     pad_vector = [0]*int(longest_sentence)
 
     for i in chapter_dict:
-        pad_vector_with_zeroes(chapter_dict[i], longest_sentence) # Normalizes vertical space to largest sentence
-        for j in range(most_sentences - len(chapter_dict[i])): # Add blank vectors to normalize width
+        pad_vector_with_zeroes(chapter_dict[i], longest_sentence)  # Normalizes vertical space to largest sentence
+        for j in range(most_sentences - len(chapter_dict[i])):  # Add blank vectors to normalize width
             chapter_dict[i].append(pad_vector)
+
+
 #         TODO: Option to alternate between append and prepend to center heatmap
 
 def main():
-    normalizing = True # Pad images to make them the same size; increases computation time quite a bit
-    making_image = True # Determines whether or not to plot the result
-    making_matrix = False # Output a .mat file to use with Octave/MATLAB
+    normalizing = True  # Pad images to make them the same size; increases computation time quite a bit
+    making_image = True  # Determines whether or not to plot the result
+    making_matrix = False  # Output a .mat file to use with Octave/MATLAB
 
     chapter_dict = {}
-    file_list = tkinter.filedialog.askopenfilenames() # Supports processing multiple files sequentially
+    file_list = tkinter.filedialog.askopenfilenames()  # Supports processing multiple files sequentially
 
     for chosen_file in file_list:
         process_to_dictionary(chosen_file, chapter_dict)
@@ -105,8 +121,8 @@ def main():
         normalize_all_maps(chapter_dict)
     if making_image:
         process_chapters_to_images(chapter_dict)
-    if making_matrix
-
+    if making_matrix:
+        process_chapters_to_matrices(chapter_dict)
 
 if __name__ == "__main__":
     main()
