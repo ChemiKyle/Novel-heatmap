66aa67e177c54b03310b0f3d75ad01d555595680
kyle <kyle.2493@gmail.com>
2017-05-08 11:12:46 -0400

    Added dictionary to prepare for normalizing option

--- book_map.py
+++ book_map.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os, tkinter, re
 
+
 def make_bookmap(book_text):
     book_map = []
     longest_line = 0
@@ -23,13 +24,18 @@ def make_bookmap(book_text):
 
     return book_map, longest_line
 
-def pad_with_zeroes(book_map, longest_line):
+def pad_vector_with_zeroes(book_map, longest_line):
     for line in book_map:
         word_count = len(line)
         if word_count < longest_line:
             for i in range(longest_line - word_count):
                 line.append(0)
 
+def make_blank_pad_vector(longest_line):
+    pad_vector = []
+    for i in range(longest_line):
+        pad_vector.append(0)
+    return pad_vector
 
 make_image = True # Determines whether or not to plot the result
 directly_to_figure = True # Set to false to produce a preview first
@@ -37,10 +43,11 @@ def numpy_image_maker(book_map, book_name):
     a = np.asarray(book_map)
     a = np.transpose(a)
     plt.axis('off')
-    cmap = 'viridis'
+    cmap = 'Blues_r'
+    dpi = 100 # Default for this with direct write is 80, but images our size don't warrant compression
     plt.imshow(a, cmap=cmap ,interpolation='nearest')
     if directly_to_figure:
-        plt.savefig(book_name + '_map_image.png', bbox_inches='tight')
+        plt.savefig(book_name + '_map_image.png', bbox_inches='tight', dpi=dpi)
     else:
         plt.show()
 
@@ -51,29 +58,43 @@ def matrix_maker(book_map, book_name):
     a = np.transpose(a)
     np.savetxt(book_name + ".mat", a, newline=";\n")
 
-
-
-def turn_text_to_output(chosen_file):
+# def normalize_sizes(list_of_bookmaps):
+def process_to_dictionary(chosen_file, chapter_dict):
     book_name, ext = os.path.splitext(chosen_file)
     with open(chosen_file, 'r') as f:
         book_text = f.read()
 
     book_map, longest_line = make_bookmap(book_text)
-    pad_with_zeroes(book_map, longest_line)
+    chapter_dict[book_name] = book_map
+    pad_vector_with_zeroes(book_map, longest_line)
+
 
     print(len(book_map))
 
-    if make_image:
-        numpy_image_maker(book_map, book_name)
-    if make_matrix:
-        matrix_maker(book_map, book_name)
+def process_chapters_to_images(chapter_dict):
+    for i in chapter_dict:
+        file_name = i
+        print(file_name)
+        book_map = chapter_dict[i]
+        numpy_image_maker(book_map, file_name)
+
+normalizing = False
+def normalize_all_maps(chapter_dict):
+    for i in chapter_dict:
+        for j in chapter_dict[i]:
+            print(len(j))
 
 def main():
+    chapter_dict = {}
     file_list = tkinter.filedialog.askopenfilenames() # Supports processing multiple files sequentially
 
     for chosen_file in file_list:
-        turn_text_to_output(chosen_file)
+        process_to_dictionary(chosen_file, chapter_dict)
 
+    if normalizing:
+        normalize_all_maps(chapter_dict)
+    if make_image:
+        process_chapters_to_images(chapter_dict)
 
 if __name__ == "__main__":
     main()
